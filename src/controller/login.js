fcf40909af0ca79b7f76067d39c9e3d90bd6c1a1
vieira <vini.clau0@gmail.com>
2023-07-13 16:09:47 -0300

    feat: req 03 - cria rota login

---
+++ src/controller/login.js
@@ -0,0 +1,32 @@
+const { UserService } = require('../service');
+const { createToken } = require('../auth/authFunctions');
+
+const isBodyValid = (username, password) => username && password;
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!isBodyValid(email, password)) {
+        return res
+            .status(400)
+            .json({ message: 'Some required fields are missing' });
+    }
+
+    const user = await UserService.getByemail(email);
+
+    if (!user || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Invalid fields' });
+    }
+    const { password: _password, ...userWithoutPassword } = user.dataValues;
+
+    const payload = { data: userWithoutPassword };
+
+    const token = createToken(payload);
+
+    return res.status(200)
+        .json({ username: user.username, token });
+};
+
+module.exports = { login };
