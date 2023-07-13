3b1094764939927c5dc86cb48e9fac7b72ddea26
vieira <vini.clau0@gmail.com>
2023-07-13 20:05:56 -0300

    feat: req 07 - cria model Category

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+const CategorySchema = (sequelize, DataTypes) => {
+    const CategoryTable = sequelize.define('Category', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        name: DataTypes.STRING,
+    },
+        {
+            tableName: 'categorys',
+            underscored: true,
+            timestamps: false,
+        },
+    );
+    return CategoryTable;
+};
+
+module.exports = CategorySchema;
